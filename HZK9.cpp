14713b90dd9eac2b6c6b90822aa4b4ecbfede21d
hello <mantm.22ite@vku.udn.vn>
2022-12-16 15:26:43 +0700

    update

--- HZK9.cpp
+++ HZK9.cpp
@@ -1,20 +1,52 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
+const int inf = 1e9 + 7;
+const int N = 1000111;
+int frefix[100005], suffix[100005];
+void print() { return; }
+void println()
 {
-    string s;
+	cout << endl;
+	return;
+}
+template <typename T, typename... Args>
+void print(T a, Args... args)
+{
+	cout << a << " ";
+	print(args...);
+}
+template <typename T, typename... Args>
+void println(T a, Args... args)
+{
+	cout << a << " ";
+	println(args...);
+}
+int32_t main(int32_t argc, char *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string s;
 	cin >> s;
-	int count0=0;
-	int count=0, max1 =0;
-	for(int i=0; i<s.length(); i++){
-		if(s[i]=='1'){
-			count++;
-		}
-		else{
-			s[i]='1';
-			count0++;
-			count0=count;
-		
+	int n = s.size();
+	s = '#' + s;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (s[i] == '1')
+		{
+			frefix[i] = frefix[i - 1] + 1;
 		}
 	}
+	for (int i = n; i >= 1; --i)
+	{
+		if (s[i] == '1')
+			suffix[i] = suffix[i + 1] + 1;
+	}
+	int res = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (s[i] == '0')
+			res = max(res, frefix[i - 1] + suffix[i + 1] + 1);
+	}
+	cout << res;
 }
